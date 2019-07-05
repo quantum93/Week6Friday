46e3d1f892f101285c3dbe5f98b65339b79c66b2
Tae Lee <taebumlee@gmail.com>
2019-07-05 15:46:33 -0700

    get doctor name by forEach

--- src/main.js
+++ src/main.js
@@ -9,14 +9,20 @@ $(document).ready(function() {
     event.preventDefault();
     let inputQuery = $('#symptom').val();
     $('#symptom').val("")
-    console.log(inputQuery);
     let service = new DoctorLookup;
     let promise = service.DoctorList();
-    console.log(promise);
 
 // ~~~~~~~~ for promise logic ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     promise.then(function (response) {
       let apiData = JSON.parse(response);
+      let doctorLists = [];
+      apiData.data.forEach(function (name) {
+        let firstName = name.profile.first_name;
+        let lastName = name.profile.last_name;
+        doctorLists.push(firstName, lastName);
+        console.log(doctorLists);
+      })
+      console.log(apiData.data);
       console.log(apiData.data[0]);
       console.log(apiData.data[0].profile);
       console.log(apiData.data[0].profile.first_name); // <---- this gives first name of doctor!
@@ -25,7 +31,7 @@ $(document).ready(function() {
       console.log(apiData.data[0].practices[0].visit_address); // <---- this gives an information about address!
       console.log(Object.keys(apiData));
       console.log(Object.keys(apiData.data)); // <------ number of doctors in query
-      console.log(Object.keys(apiData.data.profile)); // <------ number of doctors in query 
+      console.log(Object.keys(apiData.data.profile)); // <------ number of doctors in query
     }, function (error) {
       $('#doctors').text('Sorry, we cannot find Doctor for you...');
     });
