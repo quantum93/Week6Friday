a5dea19654b90c653ed118c6642d45d0cdfad855
Tae Lee <taebumlee@gmail.com>
2019-07-05 17:42:23 -0700

    html writing of query result.

--- README.md
+++ README.md
@@ -34,14 +34,17 @@
 | console.log test for first, last name, address, phone number, and new patients ....                                        |
 | using forEach, printout of doctors' name.                                                                                  |
 | it gives all doctors information by query but has a bug for missing data                                                   |
-|
-| :------------------------------------------------------ | :-------------------------- | :--------------------------------- |
+| we delete street2 option since it is not always available.                                                                 |
+| html writing polish for doctor list information.                                                                           |
 | :------------------------------------------------------ | :-------------------------- | :--------------------------------- |
 | A user should be able to to enter a name to receive a list of doctors in the Portland area that fit the search query.      |
 | :------------------------------------------------------ | :-------------------------- | :--------------------------------- |
+|
 | :------------------------------------------------------ | :-------------------------- | :--------------------------------- |
 | If the API call results in an error (any message not a 200 OK), the application should return a notification that states   |
 | what the error is.                                                                                                         |
+| :------------------------------------------------------ | :-------------------------- | :--------------------------------- |
+|
 | :------------------------------------------------------ | :-------------------------- | :--------------------------------- |
 | If the query response doesn't include any doctors (for instance, if no doctors meet the search criteria), the application  |
 | should return a notification that states that no doctors meet the criteria. (This is not an error so it should be handled  |
--- src/main.js
+++ src/main.js
@@ -19,33 +19,18 @@ $(document).ready(function() {
       apiData.data.forEach(function (name) {
         let firstName = name.profile.first_name;
         let lastName = name.profile.last_name;
-        let street2 = name.practices[0].visit_address.street2;
+        let website = name.profile.image_url;
+        // let street2 = name.practices[0].visit_address.street2;
         let street = name.practices[0].visit_address.street;
         let city = name.practices[0].visit_address.city;
         let state = name.practices[0].visit_address.state;
         let zip = name.practices[0].visit_address.zip;
         let phone = name.practices[0].phones[0].number;
         let accept = name.practices[0].accepts_new_patients;
-        console.log(firstName + lastName);
-        console.log(street2 + street + city + state + zip);
-        console.log(phone + accept);
-
-        // apiData.data.practices.forEach(function (info) {
-        //   let address = `${practice.visit_address.street}, ${info.visit_address.city} ${info.visit_address.state}, ${info.visit_address.zip}
-        //     \n${info.visit_address.street}`;
-        // });
-        // console.log(apiData.data[0].practices[0].visit_address.city);
-        // console.log(apiData.data[0].practices[0].visit_address.street);
-        // console.log(apiData.data[0].practices[0].visit_address.street2);
-        // console.log(apiData.data[0].practices[0].visit_address.zip);
-        // console.log(apiData.data[0].practices[0].visit_address.state);
-        // console.log(apiData.data[0].practices[0].accepts_new_patients);
-        // console.log(apiData.data[0].practices[0].phones[0].number);
-        // let address = name.practices.visit_address.street2 + name.practices.visit_address.street + name.practices.visit_address.city + name.practices.visit_address.state + name.practices.visit_address.zip;
-        // let phone = name.practices.phones.number;
-        // let accept = name.practices.accepts_new_patients;
-        // doctorLists.push(firstName, lastName, address, phone, website, accept);
-        // console.log(doctorLists);
+        $('#name').text(firstName + " " + lastName);
+        $('#contact').text(street + ", " + city + ", " + state + ", " + zip);
+        $('#available').text(phone + ", " + accept);
+        $('#website').text(website);
       });
 
     }, function (error) {
@@ -54,6 +39,27 @@ $(document).ready(function() {
 // ~~~~~~~~ for promise logic ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   });
 });
+// console.log(firstName + lastName);
+// console.log(website);
+// console.log(street + city + state + zip);
+// console.log(phone + accept);
+
+// apiData.data.practices.forEach(function (info) {
+  //   let address = `${practice.visit_address.street}, ${info.visit_address.city} ${info.visit_address.state}, ${info.visit_address.zip}
+  //     \n${info.visit_address.street}`;
+  // });
+  // console.log(apiData.data[0].practices[0].visit_address.city);
+  // console.log(apiData.data[0].practices[0].visit_address.street);
+  // console.log(apiData.data[0].practices[0].visit_address.street2);
+  // console.log(apiData.data[0].practices[0].visit_address.zip);
+  // console.log(apiData.data[0].practices[0].visit_address.state);
+  // console.log(apiData.data[0].practices[0].accepts_new_patients);
+  // console.log(apiData.data[0].practices[0].phones[0].number);
+  // let address = name.practices.visit_address.street2 + name.practices.visit_address.street + name.practices.visit_address.city + name.practices.visit_address.state + name.practices.visit_address.zip;
+  // let phone = name.practices.phones.number;
+  // let accept = name.practices.accepts_new_patients;
+  // doctorLists.push(firstName, lastName, address, phone, website, accept);
+  // console.log(doctorLists);
 
 // console.log(apiData.data);
 // console.log(apiData.data[0]);
