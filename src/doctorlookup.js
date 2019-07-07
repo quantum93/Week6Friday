ff4adb26e94958fbad5a76cec5d19aceaa6d26be
Tae Lee <taebumlee@gmail.com>
2019-07-06 18:55:04 -0700

    try to fix list problem but still doesn't work

--- src/doctorlookup.js
+++ src/doctorlookup.js
@@ -2,11 +2,20 @@ class DoctorLookup{
   DoctorList(firstname, lastname, inputQuery) {
     return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstname}&last_name=${lastname}&query=${inputQuery}&location=45.523%2C%20-122.676%2C100&user_location=45.523%2C%20-122.676&skip=0&limit=10&user_key=${process.env.apiKey}`; // <----- use `` rather than '' to enclose https information
-      console.log(typeof url)
+      let url = "";
+      if (firstname === "" && lastname === "" && inputQuery === "") {
+        url = `https://api.betterdoctor.com/2016-03-01/doctors?&location=45.523%2C%20-122.676%2C100&user_location=45.523%2C%20-122.676&user_key=${process.env.apiKey}`; // <----- use `` rather than '' to enclose https information
+      }
+      if (firstname === "" && lastname === "" && inputQuery !== "") {
+        url = `https://api.betterdoctor.com/2016-03-01/doctors?&query=${inputQuery}&location=45.523%2C%20-122.676%2C100&user_location=45.523%2C%20-122.676&user_key=${process.env.apiKey}`; // <----- use `` rather than '' to enclose https information
+      }
+      if (firstname !== "" && lastname !== "" && inputQuery === "") {
+        url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstname}&last_name=${lastname}&location=45.523%2C%20-122.676%2C100&user_location=45.523%2C%20-122.676&user_key=${process.env.apiKey}`; // <----- use `` rather than '' to enclose https information
+      }
+      // const url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstname}&last_name=${lastname}&query=${inputQuery}&location=45.523%2C%20-122.676%2C100&user_location=45.523%2C%20-122.676&skip=0&limit=10&user_key=${process.env.apiKey}`; // <----- use `` rather than '' to enclose https information
+
       // const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.523064%2C%20-122.676483%2C100&user_location=portland&skip=0&limit=10&user_key=ffe395fbf97c561b518013a794f3aba9=${process.env.apiKey}`; // <-- test for error in API query by promise
       request.onload = function () {
-        console.log(this.firstname)
         if (this.status === 200) {
           resolve(request.response);
         } else {
--- src/main.js
+++ src/main.js
@@ -8,8 +8,6 @@ $(document).ready(function() {
   $('#myInfo').submit(function(event) {
     event.preventDefault();
     let inputFirstName = $('#firstname').val();
-    console.log(typeof inputFirstName)
-    console.log(inputFirstName)
     $('#firstname').val("")
     let inputlLastName = $('#lastname').val();
     $('#lastname').val("")
@@ -21,31 +19,27 @@ $(document).ready(function() {
 // ~~~~~~~~ for promise logic ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     promise.then(function (response) {
       let apiDoctors = JSON.parse(response);
-      apiDoctors.data.forEach(function (name) {
-        // doesn't match anything means the length of data is zero.
-        if (name.length === 0) {
-          $('#doctors').append('Sorry, no doctors for this search and please try again');
-        }
-        // if not, we try to catch the data from API
-        let doctorName = name.profile.first_name + " " + name.profile.last_name;
-        let doctorAddress = "";
-        if (name.practices[0].visit_address.street2 !== undefined) {
-          doctorAddress = name.practices[0].visit_address.street2 + " " + name.practices[0].visit_address.street + " " + name.practices[0].visit_address.city + " " + name.practices[0].visit_address.state + " " + name.practices[0].visit_address.zip
-        } else {
-          doctorAddress = name.practices[0].visit_address.street + " " + name.practices[0].visit_address.city + " " + name.practices[0].visit_address.state + " " + name.practices[0].visit_address.zip
-        }
-        // let street2 = name.practices[0].visit_address.street2;
-
-        let phone = name.practices[0].phones[0].number;
-        let website = name.profile.image_url;
-        let reservation = "";
-        if (name.practices[0].accepts_new_patients === true) {
-          reservation = ':available doctor for appointment';
-        } else {
-          reservation = ":not available doctor for appointment"
-        }
-        $('#doctors').append(doctorName + ": " + doctorAddress + " Tel.: " + phone + " " + reservation + " " + website + "</br>");
-      });
+      if (apiDoctors.meta.total == 0) {
+        $('#doctors').append('Sorry, no doctors for this search and please try again');
+      }
+      // apiDoctors.data.forEach(function (name) {
+      //   let doctorName = name.profile.first_name + " " + name.profile.last_name;
+      //   let doctorAddress = "";
+      //   if (name.practices[0].visit_address.street2 !== undefined) {
+      //     doctorAddress = name.practices[0].visit_address.street2 + " " + name.practices[0].visit_address.street + " " + name.practices[0].visit_address.city + " " + name.practices[0].visit_address.state + " " + name.practices[0].visit_address.zip
+      //   } else {
+      //     doctorAddress = name.practices[0].visit_address.street + " " + name.practices[0].visit_address.city + " " + name.practices[0].visit_address.state + " " + name.practices[0].visit_address.zip
+      //   }
+      //   let phone = name.practices[0].phones[0].number;
+      //   let website = name.profile.image_url;
+      //   let reservation = "";
+      //   if (name.practices[0].accepts_new_patients === true) {
+      //     reservation = ':available doctor for appointment';
+      //   } else {
+      //     reservation = ":not available doctor for appointment"
+      //   }
+      //   $('#doctors').append(doctorName + ": " + doctorAddress + " Tel.: " + phone + " " + reservation + " " + website + "</br>");
+      // });
     }, function (error) {
       $('#doctors').text('Sorry, there is an error to process...');  // <---- this is for error of process by promise function.
     });
